79f8681be20f373904fade4c56f9d3209a4667f5
Toren Wallengren <toren.wallengren@addepar.com>
2024-07-07 14:27:09 +0100

    docstring

--- data_generator.py
+++ data_generator.py
@@ -50,6 +50,15 @@ class DataGenerator:
 
     def generate_random_price_vectors(self, num_of_assets, num_of_days, start_price_range=(75, 100),
                                       distribution='poisson'):
+        """
+        Price vectors are of length num_of_days + 1. The first element of each price vector is set to 1/num_of_assets.
+        The remaining elements are generated using random walks.
+        :param num_of_assets:
+        :param num_of_days:
+        :param start_price_range: generates uniform random start prices in this range
+        :param distribution: the type of distribution used for generating random walks
+        :return:
+        """
         price_vectors = np.full((num_of_assets, num_of_days + 1), 1 / num_of_assets)
         start_prices = self.rng.integers(*start_price_range, num_of_assets)
         random_walks = self.generate_random_walk(start_prices, num_of_days, distribution)
@@ -59,6 +68,17 @@ class DataGenerator:
 
     def generate_random_portfolio_value_vector(self, num_of_days, start_value=10000, distribution='normal',
                                                mu=0, sigma=100, lam=1):
+        """
+        Portfolio value vectors are of length num_of_days + 1. The first element of each portfolio value vector is set
+        to one, and the remaining elements are generated using start value and a random walk.
+        :param num_of_days:
+        :param start_value:
+        :param distribution:
+        :param mu:
+        :param sigma:
+        :param lam:
+        :return:
+        """
         initial_values = [start_value]
         value_vector = np.ones(num_of_days + 1)
         random_walk = self.generate_random_walk(initial_values, num_of_days, distribution, mu, sigma, lam)[0]
